4c50bc847d91b2e823095d20624f6f66db3edd38
Max Joseph <maxwellbjoseph@gmail.com>
2019-10-04 11:15:25 -0600

    For hurricanes too

--- R/hotspots.R
+++ R/hotspots.R
@@ -5,4 +5,5 @@ tornadoes <- raster('output/tornado_hazard.tif')
 hurricanes <- raster('output/hurricane_hazard.tif')
 
 tq <- quantile(tornadoes, .9)
-plot(tornadoes > quantile(tornadoes, .9))
+hq <- quantile(hurricanes, .9)
+plot(tornadoes > tq)
