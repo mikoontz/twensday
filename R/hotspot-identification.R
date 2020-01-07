c9ffdb2f39b338e2cc0907796b1cbef7696ca99d
Michael Koontz <mikoontz@gmail.com>
2020-01-06 17:56:51 -0700

    identify hazard hotspots from the standardized hazard layers (and the multihazard layer)

---
+++ R/hotspot-identification.R
@@ -0,0 +1,15 @@
+# Purpose: Identify some hotspots for hazards and multihazards in CONUS
+
+library(tidyverse)
+library(data.table)
+library(raster)
+library(fasterize)
+
+source("R/download_grid.R")
+
+empty_grid <-
+  download_grid() %>% 
+  raster::raster()
+
+multihazard_dt <- data.table::fread("output/multihazard-ecdf_conus.csv")
+
