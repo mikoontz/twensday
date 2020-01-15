25fa4a7de89dd6dc21837f07f5417f9fdf3ac29a
Michael Koontz <mikoontz@gmail.com>
2020-01-15 11:45:46 -0700

    use the original hurricane wind as the source (rather than the partially reprojected version) and convert to probability of an event happening in one year given a rate of the event (number of hurricane landfalls divided by 21 year record)

--- R/configure-hurricane-wind.R
+++ R/configure-hurricane-wind.R
@@ -23,16 +23,15 @@ empty_grid <-
 # These set up the variables to be used to get the hazard data and name
 # new output files appropriately
 hazard_name <- "hurricane-wind"
-hazard_file <- "CycloneFrequency_1980_2000_projected/gdcyc_NAD.tif"
-zip_path <- file.path("data", "hazards", "CycloneFrequency_1980_2000_projected.zip")
-
-# The hurricane wind data is on the Google Drive
-hazard_id <- "1REzIWNeq4zwwZdiTT2YBa7UYXTYA-r2s"
+# hazard_file <- "CycloneFrequency_1980_2000_projected/gdcyc_NAD.tif"
+# zip_path <- file.path("data", "hazards", "CycloneFrequency_1980_2000_projected.zip")
+# 
+# # The hurricane wind data is on the Google Drive
+# hazard_id <- "1REzIWNeq4zwwZdiTT2YBa7UYXTYA-r2s"
 
 hazard_file <- "gdcyc/gdcyc.asc"
 zip_path <- file.path("data", "hazards", "gdcyc_cyclone.zip")
 hazard_id <- "1whh-JSmF7v6vJm35lgQAAt5bs01Phb_t"
-
 
 # Names of the files (to read and manipulate, and then what to call it upon
 # export)
@@ -71,12 +70,7 @@ if(!file.exists(hazard_path_out) | overwrite) {
   
   unlink(hazard_path_tmp)
   
-  # This source represents records for 21 years (Jan 1, 1980 to Dec 31, 2000)
-  # https://sedac.ciesin.columbia.edu/data/set/ndh-cyclone-hazard-frequency-distribution
-  # One caveat is that there is a mask applied to 1 km grid cells to exclude
-  # cells with <5 people per square km or without significant agriculture
-  
-  # Make 0/NA handling consistent by using a 0 within CONUS for "no hazard"
+    # Make 0/NA handling consistent by using a 0 within CONUS for "no hazard"
   hazard <- 
     rasterDT::subsDT(x = hazard, dict = data.table(by = NA, which = 0), subsWithNA = FALSE)
 
@@ -91,8 +85,20 @@ if(!file.exists(hazard_path_out) | overwrite) {
   mask <- raster::raster("output/water-mask_zillow-grid.tif")
   hazard <- raster::mask(x = hazard, mask = mask)
   
+  # This source represents records for 21 years (Jan 1, 1980 to Dec 31, 2000)
+  # https://sedac.ciesin.columbia.edu/data/set/ndh-cyclone-hazard-frequency-distribution
+  # One caveat is that there is a mask applied to 1 km grid cells to exclude
+  # cells with <5 people per square km or without significant agriculture
+  hazard_rate <- hazard / 21
+  # assume an exponential distribution of waiting times, and then we can use
+  # the cumulative distribution function for the exponential to ask what
+  # is the probability that an event occurred before a specified waiting time
+  # (one year, in our case) given a rate of the hazard (which we figured 
+  # empirically)
+  hazard_prob <- 1 - exp(-1 * hazard_rate)
+  
   # write to disk
-  raster::writeRaster(x = hazard, filename = hazard_path_out, overwrite = TRUE)
+  raster::writeRaster(x = hazard_prob, filename = hazard_path_out, overwrite = TRUE)
   
 }
 
