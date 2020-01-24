c02d4ddd5b6f807111ecccf6588438339769b41d
Michael Koontz <mikoontz@gmail.com>
2020-01-24 12:09:06 -0700

    expand the smoothing window to 150km

--- R/configure-tornado.R
+++ R/configure-tornado.R
@@ -7,6 +7,7 @@ library(magrittr)
 library(tidyverse)
 library(USAboundaries)
 library(velox)
+library(gdalUtils)
 
 source("R/download_grid.R")
 
@@ -29,11 +30,6 @@ overwrite <- FALSE
 
 if(!file.exists(hazard_path_out) | overwrite) {
   if(!file.exists(hazard_path_unmasked) | overwrite) {
-    conus <- 
-      USAboundaries::us_boundaries(type = "state", resolution = "high") %>%
-      filter(!state_name %in% c("Alaska", "Hawaii") & jurisdiction_type == "state") %>%
-      st_transform(projection(empty_grid))
-    
     # Get the tornado data
     tornado_paths <- tornadoes() %>%
       as('sf') %>%
@@ -50,43 +46,98 @@ if(!file.exists(hazard_path_out) | overwrite) {
     
     # buffer at 300 m
     # https://journals.ametsoc.org/doi/pdf/10.1175/JAMC-D-13-0235.1
-    buffered_paths <- st_buffer(tornado_paths, dist = 300)
+    # Fig. 6 showing mean tornado path widths approx 300m
+    buffered_paths <- 
+      st_buffer(tornado_paths, dist = 300)
     
     # Compute an annual empirical frequency for each grid cell
     # (# years with events / total # years)
     # and save to a tif file
+    
+    conus <- 
+      USAboundaries::us_boundaries(type = "state", resolution = "high") %>%
+      filter(!state_name %in% c("Alaska", "Hawaii") & jurisdiction_type == "state") %>% 
+      st_transform(projection(empty_grid))
+    
     tornado_counts <- buffered_paths %>%
-      group_by(yr) %>%
-      summarize %>% 
-      st_cast("MULTIPOLYGON") %>%
       fasterize(raster = empty_grid, fun = 'sum', background = 0)
     
+    tornado_rate <- tornado_counts / n_year
+    # assume an exponential distribution of waiting times, and then we can use
+    # the cumulative distribution function for the exponential to ask what
+    # is the probability that an event occurred before a specified waiting time
+    # (one year, in our case) given a rate of the hazard (which we figured 
+    # empirically)
+    tornado_prob <- 1 - exp(-1 * tornado_rate)
     
-    tornado_freq <- tornado_counts / n_year
-    plot(tornado_freq)
+    raster::writeRaster(x = tornado_prob, 
+                        filename = file.path("output", "hazards", "tornado-prob_raw.tif"),
+                        overwrite = TRUE)
+    
+    # Aggregate to 1000m so we can use a smoother with a larger radius (thus fewer pixels in
+    # each focal operation)
+    
+    gdalwarp(srcfile = file.path("output", "hazards", "tornado-prob_raw.tif"), 
+             dstfile = file.path("output", "hazards", "tornado-prob_1000.tif"), 
+             t_srs = crs(empty_grid), 
+             tr = c(1000, 1000), 
+             overwrite = TRUE,
+             r = "bilinear")
+    
+    tornado_prob_1000 <- raster::raster(file.path("output", "hazards", "tornado-prob_1000.tif"))
     
     # Smoothing
-    gf <- focalWeight(tornado_freq, 10000, "circle")
-    vx <- velox::velox(x = tornado_freq)
+    # Use 150 km radius for smoothing: https://doi.org/10.1175/2013EI000559.1
+    # Kernel shape less important than the radius used: https://doi.org/10.1175/BAMS-D-11-00219.1
+    # We set sigma to the desired radius divided by 3; the default radius is sigma times 3
+    # if the radius represents 3 sigmas from the mean, then 99.7% of the mass of the Gaussian kernel
+    # will be contained in the bounds of the radius; 
+    # still would be considered a 'bounded Gaussian' distribution though.
+    gf <- focalWeight(tornado_prob_1000, 150000 / 3, "Gauss")
+    vx <- velox::velox(x = tornado_prob_1000)
     
-    # meanFocal operation takes about 38 minutes on the Alienware. 
+    # meanFocal operation takes about 40 minutes on the Alienware. 
     # Velox should be quite a bit faster than the raster::focal()
     # implementation. See http://philipphunziker.com/velox/benchmark.html
     vx$meanFocal(weights = gf)
     rg <- vx$as.RasterLayer()
     
-    plot(rg, col = viridis::viridis(100))
+    # Mask out the ocean prior to scaling
+    ocean_mask_1000 <- fasterize::fasterize(sf = conus, raster = tornado_prob_1000)
+    ocean_mask_250 <- fasterize::fasterize(sf = conus, raster = tornado_prob)
     
-    rg_01 <- rg / cellStats(rg, stat = "sum")
-    rg_scaled <- rg_01 * cellStats(tornado_freq, stat = "sum")
+    rg_ocean_masked <- raster::mask(x = rg, mask = ocean_mask_1000)
     
-    cellStats(tornado_freq, stat = "sum")
-    cellStats(rg_scaled, stat = "sum")
+    raster::writeRaster(x = rg_ocean_masked, filename = hazard_path_unmasked, overwrite = TRUE)
+
+    hazard_path_tmp <- file.path("data", "hazards", hazard_name, paste0(hazard_name, "_temp.tif"))
     
-    raster::writeRaster(x = rg_scaled, filename = hazard_path_unmasked)
+    gdalwarp(srcfile = hazard_path_unmasked, 
+             dstfile = hazard_path_tmp, 
+             t_srs = crs(empty_grid), 
+             tr = c(250, 250), 
+             overwrite = TRUE,
+             r = "bilinear")
+    
+    hazard <- 
+      gdalUtils::align_rasters(unaligned = hazard_path_tmp, 
+                               reference = empty_grid@file@name, 
+                               output_Raster = TRUE)
+    
+    tornado_prob_ocean_masked <- raster::mask(x = tornado_prob, mask = ocean_mask_250)
+    
+    # rescale the values of the cells such that the total probability across CONUS
+    hazard_01 <- hazard / cellStats(hazard, stat = "sum")
+    hazard_scaled <- hazard_01 * cellStats(tornado_prob_ocean_masked, stat = "sum")
+    
+    cellStats(rg_ocean_masked, stat = "sum")
+    cellStats(hazard, stat = "sum")
+    cellStats(tornado_prob_ocean_masked, stat = "sum")
+    cellStats(hazard_scaled, stat = "sum")
+    
+    raster::writeRaster(x = hazard_scaled, filename = hazard_path_unmasked, overwrite = TRUE)
+    
   }
-  
-  hazard <- raster::raster(hazard_path_unmasked)
   
   # Mask out the pixels outside of CONUS using the water mask derived from the 
   # USAboundaries package high resolution CONUS shapefile (rasterized to the Zillow
@@ -97,7 +148,11 @@ if(!file.exists(hazard_path_out) | overwrite) {
   }
   
   mask <- raster::raster("output/water-mask_zillow-grid.tif")
-  hazard <- raster::mask(x = hazard, mask = mask)
+  
+  # overwrite R object `hazard` to be the newly aligned and warped layer
+  hazard <-
+    raster::raster(hazard_path_unmasked) %>% 
+    raster::mask(mask = mask)
   
   writeRaster(x = hazard, 
               filename = hazard_path_out, 
